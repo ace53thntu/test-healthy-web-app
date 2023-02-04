94e1dcdef080d2e199ef1c520b7ba0352b96f2d5
Em Dinh <ace53thntu@gmail.com>
2023-02-04 16:23:06 +0700

    tailwind css setup, add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import './App.css';
-
-import { useState } from 'react';
-
-import reactLogo from './assets/react.svg';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React 111</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="app">
+      <h1 className="text-sm">
+        魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ…
+      </h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        light: '#FFFFFF',
+        'dark-500': '#414141',
+        'dark-600': '#2E2E2E',
+        'gray-400': '#777777',
+        'primary-300': '#FFCC21',
+        'primary-400': '#FF963C',
+        'primary-500': '#EA6C00',
+        'primary-300-400': 'linear-gradient(180deg, #FFCC21 0%, #FF963C 100%)',
+        'secondary-300': '#8FE9D0',
+        'menu-divide-top': 'rgb(255 255 255 / 15%)',
+        'menu-divide-bottom': 'rgb(46 46 46 / 25%)',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        notosansjp: ['Noto Sans JP', 'sans-serif'],
+      },
+      backgroundImage: {
+        'gradient-yellowred':
+          'linear-gradient(180deg, #FFCC21 0%, #FF963C 100%)',
+      },
+    },
+    screens: {
+      md: '960px',
+    },
+  },
+  plugins: [],
+};
